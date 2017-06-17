e96e14019127a23425876ccfd5c1b29358f2a03c
davewasthere <davebeer@gmail.com>
2017-06-17 13:26:33 +1000

    initial commit

---
+++ Foodshare/DAL/DbContext.cs
@@ -0,0 +1,25 @@
+﻿using Foodshare.Models;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Foodshare.DAL
+{
+    public class DbContext : IdentityDbContext<ApplicationUser>
+    {
+        public DbContext() : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static DbContext Create()
+        {
+            return new DbContext();
+        }
+
+        public DbSet<Supplier> Suppliers { get; set; }
+
+    }
+}
---
+++ Foodshare/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Foodshare.Startup))]
+namespace Foodshare
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
