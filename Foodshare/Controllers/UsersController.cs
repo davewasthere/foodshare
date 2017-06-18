fc92d52bf0a1c35a5d427117e55ef81bad02d72c
davewasthere <davebeer@gmail.com>
2017-06-18 10:03:42 +1000

    users

---
+++ Foodshare/Controllers/UsersController.cs
@@ -0,0 +1,23 @@
+﻿using Foodshare.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Foodshare.Controllers
+{
+    [Authorize]
+    public class UsersController : Controller
+    {
+        private FoodshareDbContext db = new FoodshareDbContext();
+
+        // GET: Users
+        public ActionResult Index()
+        {
+            var users = db.Users.ToList();
+
+            return View(users);
+        }
+    }
+}
